9aa50aaa67eabdbc3fd6a09ca4fd2e946f679c72
Jennifer Gomez <jennifergomezbruno@gmail.com>
2020-02-04 11:03:32 -0500

    first commit

--- js/index.js
+++ js/index.js
@@ -1,1 +1,44 @@
-document.addEventListener("DOMContentLoaded", function() {});
+document.addEventListener("DOMContentLoaded", function() {
+    console.log('DOM CONNECTED')
+    getAllBooks()
+    getListItem().addEventListener('click', renderShowPanel)
+});
+
+function getAllBooks() {
+    fetch("http://localhost:3000/books")
+    .then(response => response.json())
+    .then(data => data.forEach(book => renderBookTitles(book)))
+}
+
+function renderBookTitles(book) {
+    let listPanel = document.getElementById('list')
+    let listItem = document.createElement('li')
+    listItem.className += "book-titles"
+    listItem.innerText = book.title
+
+    listItem.dataset.id = book.id
+
+    listPanel.appendChild(listItem)
+
+}
+
+function renderShowPanel(event){
+    bookId = event.target.dataset.id
+    fetch("http://localhost:3000/books/" + bookId)
+    .then(response => response.json())
+    .then(data => showPanelCard(data))
+}
+
+function showPanelCard(data){
+    let showPanel = document.getElementById('show-panel')
+    let showCard = document.createElement('div')
+    showPanel.appendChild(showCard)
+    
+    let title = document.createElement('h2')
+    title.innerText = data.title
+    showCard.appendChild(title)
+}
+
+function getListItem() {
+    return document.getElementById('list')
+}
